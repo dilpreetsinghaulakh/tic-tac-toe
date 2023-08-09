5dc956064652a08531295b433dde85d2dedbd985
Dilpreet Singh <dilpreetsinghaulakh@icloud.com>
2023-08-09 22:00:55 +0530

    Add checkWin func to check if player won.

--- script.js
+++ script.js
@@ -19,10 +19,13 @@ const gameBoard = (() => {
     } else {
       gameBoardArray[position] = "O";
     }
-    console.log(gameBoardArray);
-
     return gameBoardArray;
   };
+
+  // const checkPosition = (letter) => {
+  //   let field = gameBoardArray.map((a) => a === letter);
+  //   return field;
+  // };
   return { playerInput, newGame };
 })();
 
@@ -38,14 +41,29 @@ const updatePlayer = () => {
   else player--;
 };
 
+var xPositions = [];
+var oPositions = [];
+
 const onPlayerInput = (position) => {
   gameBoard.playerInput(player, position);
+
   var playerSign = "";
-  if (player === 1) playerSign = "X";
-  else playerSign = "O";
+  if (player === 1) {
+    playerSign = "X";
+    xPositions.push(position);
+  } else {
+    playerSign = "O";
+    oPositions.push(position);
+  }
+  console.log(oPositions.length);
+  console.log(xPositions.length);
+  if (oPositions.length > 2 || xPositions.length > 2) {
+    checkWin();
+  }
+
   updatePlayer();
 
-  return playerSign;
+  return { playerSign, xPositions, oPositions };
 };
 
 const createGrid = () => {
@@ -57,11 +75,42 @@ const createGrid = () => {
     gridContainer.appendChild(gridItem);
 
     gridItem.addEventListener("click", () => {
-      let position = gridItem.id;
-      playerSign = onPlayerInput(position);
+      let position = parseInt(gridItem.id, 10);
+      playerSign = onPlayerInput(position).playerSign;
       gridItem.textContent = playerSign;
+      // console.log(gameBoard.checkPosition("X"))
     });
   }
 };
 
 playGame();
+
+const winCases = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+const checkWin = () => {
+  const containsAll = (position, winArr) => {
+    console.log(position);
+    console.log(winArr);
+    return winArr.every((element) => position.includes(element));
+  };
+  if (player === 2) {
+    console.log(oPositions);
+    winCases.forEach((array) => {
+      console.log(containsAll(oPositions, array));
+    });
+  } else {
+    console.log(xPositions);
+    winCases.forEach((array) => {
+      console.log(containsAll(xPositions, array));
+    });
+  }
+};
